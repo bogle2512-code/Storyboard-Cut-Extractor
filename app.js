f4381ff3f790f2214400fb6878f71f2c8c42b744
bogle2512-code <bogle2512@gmail.com>
2026-05-16 10:38:50 +0900

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-if ("serviceWorker" in navigator && location.protocol !== "file:") {
-  navigator.serviceWorker.register("./service-worker.js").catch(() => {});
-}
-
-const script = document.createElement("script");
-script.src = "./src/standalone.js";
-script.defer = false;
-document.body.appendChild(script);
-
